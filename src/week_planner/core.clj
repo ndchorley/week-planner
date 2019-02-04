b2a546960bca13aa4976ee8143cd59186c7e1ced
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-04 08:48:18 +0000

    Use Hiccup's HTML5 support

--- src/week_planner/core.clj
+++ src/week_planner/core.clj
@@ -1,15 +1,14 @@
 (ns week-planner.core (:gen-class)
     (:require [ring.adapter.jetty :as jetty]
-              [hiccup2.core :as hiccup]))
+              [hiccup.page :as hiccup]))
 
 (defn plan [request]
   {:status 200
    :body (str
-          (hiccup/html
-           [:html
-            [:head
-             [:title "Week plan"]]
-            [:body "Week plan will go here"]]))})
+          (hiccup/html5
+           [:head
+            [:title "Week plan"]]
+           [:body "Week plan will go here"]))})
 
 (defn -main [& args]
   (jetty/run-jetty plan {:port (Integer/parseInt (System/getenv "PORT"))}))
