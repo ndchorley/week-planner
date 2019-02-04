9e397495f913e00bb4cb4901e087794e5a5ed2e4
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-04 08:38:33 +0000

    Add an HTML page

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject week-planner "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [hiccup "2.0.0-alpha2"]]
   :min-lein-version "2.0.0"
   :main week-planner.core
   :aot [week-planner.core]
--- src/week_planner/core.clj
+++ src/week_planner/core.clj
@@ -1,9 +1,15 @@
 (ns week-planner.core (:gen-class)
-    (:require [ring.adapter.jetty :as jetty]))
+    (:require [ring.adapter.jetty :as jetty]
+              [hiccup2.core :as hiccup]))
 
 (defn plan [request]
   {:status 200
-   :body "Week plan will go here"})
+   :body (str
+          (hiccup/html
+           [:html
+            [:head
+             [:title "Week plan"]]
+            [:body "Week plan will go here"]]))})
 
 (defn -main [& args]
   (jetty/run-jetty plan {:port (Integer/parseInt (System/getenv "PORT"))}))
