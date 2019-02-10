ce43c1d4d9053e23a6ed5b66c7d8a4efc10adf1f
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-10 09:27:47 +0000

    Move planning logic to its own namespace

--- src/week_planner/core.clj
+++ src/week_planner/core.clj
@@ -1,33 +1,12 @@
 (ns week-planner.core (:gen-class)
     (:require [ring.adapter.jetty :as jetty]
-              [hiccup.page :as hiccup]
-              [week-planner.ramblers :as ramblers]
               [week-planner.handler :as handler]
-              [java-time]))
-
-(declare make-plan)
+              [week-planner.plan :as plan]))
 
 (defn -main [& args]
   (jetty/run-jetty
-   (partial handler/plan make-plan)
+   (partial handler/plan plan/make-plan)
    {:port (Integer/parseInt
            (if (nil? (System/getenv "PORT"))
              "9000"
              (System/getenv "PORT")))}))
-
-(declare in-week-ahead?)
-
-(defn make-plan []
-  (def events (sort-by :date-time (ramblers/get-events)))
-  (def today (java-time/local-date))
-
-  (into
-   (sorted-map)
-   (group-by
-    (fn [e] (java-time/local-date (:date-time e)))
-    (take-while in-week-ahead? events))))
-
-(defn in-week-ahead? [event]
-  (java-time/before?
-   (java-time/local-date (:date-time event))
-   (java-time/plus today (java-time/days 8))))
---
+++ src/week_planner/plan.clj
@@ -0,0 +1,20 @@
+(ns week-planner.plan
+  (:require [java-time]
+            [week-planner.ramblers :as ramblers]))
+
+(declare in-week-ahead?)
+
+(defn make-plan []
+  (def events (sort-by :date-time (ramblers/get-events)))
+  (def today (java-time/local-date))
+
+  (into
+   (sorted-map)
+   (group-by
+    (fn [e] (java-time/local-date (:date-time e)))
+    (take-while in-week-ahead? events))))
+
+(defn- in-week-ahead? [event]
+  (java-time/before?
+   (java-time/local-date (:date-time event))
+   (java-time/plus today (java-time/days 8))))
