a8f25dd8a11842168d38b237352af386e0018713
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-10 09:58:35 +0000

    Move `today` into `in-week-ahead?`

--- src/week_planner/plan.clj
+++ src/week_planner/plan.clj
@@ -6,7 +6,6 @@
 
 (defn make-plan []
   (def events (sort-by :date-time (ramblers/get-events)))
-  (def today (java-time/local-date))
 
   (into
    (sorted-map)
@@ -15,6 +14,7 @@
     (take-while in-week-ahead? events))))
 
 (defn- in-week-ahead? [event]
+  (def today (java-time/local-date))
   (java-time/before?
    (java-time/local-date (:date-time event))
    (java-time/plus today (java-time/days 8))))
