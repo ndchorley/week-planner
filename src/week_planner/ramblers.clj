b31cfa82b3e32533e8cd037b238d95591beb518a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-10 10:59:12 +0000

    Use `let` instead of `def` in `ramblers`

--- src/week_planner/ramblers.clj
+++ src/week_planner/ramblers.clj
@@ -9,26 +9,24 @@
   (flatten
    (map
     (fn [gid]
-      (def document (.get (Jsoup/connect
-                           (str "https://www.ramblers.org.uk/find-a-walk.aspx?layer=walks&tab=walks&group=" gid))))
-
-      (def elements (.getElementsByClass document "details"))
-
-      (map to-event elements))
+      (let [document (.get (Jsoup/connect
+                            (str "https://www.ramblers.org.uk/find-a-walk.aspx?layer=walks&tab=walks&group=" gid)))
+            elements (.getElementsByClass document "details")]
+        (map to-event elements)))
     #{"IL50" "ES50"})))
 
 (defn- to-event [element]
-  (def a-element (first (.getElementsByTag element "a")))
-  (def subtitles (.getElementsByClass element "subtitle"))
+  (let [a-element (first (.getElementsByTag element "a"))
+        subtitles (.getElementsByClass element "subtitle")
 
-  (def date-time (java-time/local-date-time
-                  (java-time/formatter "EEEE, d MMMM uuuu H:mm")
-                  (str
-                   (str/replace (.text (first subtitles)) (re-pattern " \\(.*\\)") "")
-                   (str/replace (.text (second subtitles)) "Start time" ""))))
+        date-time (java-time/local-date-time
+                   (java-time/formatter "EEEE, d MMMM uuuu H:mm")
+                   (str
+                    (str/replace (.text (first subtitles)) (re-pattern " \\(.*\\)") "")
+                    (str/replace (.text (second subtitles)) "Start time" "")))
 
-  (def route (.text a-element))
+        route (.text a-element)]
 
-  {:url (.get (.attributes a-element) "href")
-   :title (str route ", " (.text (last subtitles)))
-   :date-time date-time})
+    {:url (.get (.attributes a-element) "href")
+     :title (str route ", " (.text (last subtitles)))
+     :date-time date-time}))
