6b4e89907a67967d5ca7ca82f78e994cd02dc8b6
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2019-02-10 09:20:44 +0000

    Move handler to its own namespace

--- src/week_planner/core.clj
+++ src/week_planner/core.clj
@@ -2,44 +2,18 @@
     (:require [ring.adapter.jetty :as jetty]
               [hiccup.page :as hiccup]
               [week-planner.ramblers :as ramblers]
+              [week-planner.handler :as handler]
               [java-time]))
 
-(declare plan make-plan)
+(declare make-plan)
 
 (defn -main [& args]
   (jetty/run-jetty
-   (partial plan make-plan)
+   (partial handler/plan make-plan)
    {:port (Integer/parseInt
            (if (nil? (System/getenv "PORT"))
              "9000"
              (System/getenv "PORT")))}))
-
-(declare to-para)
-
-(defn plan [make-plan request]
-  {:status 200
-   :body (str
-          (hiccup/html5
-           [:head
-            [:title "Week plan"]]
-           [:body
-            [:h1 "Week plan"]
-            (map
-             (fn [date-events]
-               (def date (first date-events))
-               (def events (last date-events))
-
-               (cons
-                [:p [:b (java-time/format (java-time/formatter "EEEE, d MMMM") date)]]
-                (map to-para events)))
-             (make-plan))]))})
-
-(defn to-para [event]
-  [:p
-   (java-time/format (java-time/formatter "H:mm") (:date-time event)) " "
-   (:title event) " "
-   [:a {:href (:url event)} "More info"]])
-
 
 (declare in-week-ahead?)
 
---
+++ src/week_planner/handler.clj
@@ -0,0 +1,29 @@
+(ns week-planner.handler
+  (:require [hiccup.page :as hiccup]
+            [java-time]))
+
+(declare to-para)
+
+(defn plan [make-plan request]
+  {:status 200
+   :body (str
+          (hiccup/html5
+           [:head
+            [:title "Week plan"]]
+           [:body
+            [:h1 "Week plan"]
+            (map
+             (fn [date-events]
+               (def date (first date-events))
+               (def events (last date-events))
+
+               (cons
+                [:p [:b (java-time/format (java-time/formatter "EEEE, d MMMM") date)]]
+                (map to-para events)))
+             (make-plan))]))})
+
+(defn- to-para [event]
+  [:p
+   (java-time/format (java-time/formatter "H:mm") (:date-time event)) " "
+   (:title event) " "
+   [:a {:href (:url event)} "More info"]])
